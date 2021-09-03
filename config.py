e82aec4973afd9dc99047cfca4324b2014cccb42
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-09-03 19:59:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
 QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/fa5805751e44608b1e162.png")
 AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/fa5805751e44608b1e162.png")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 admins = {}
 API_ID = int(getenv("API_ID"))

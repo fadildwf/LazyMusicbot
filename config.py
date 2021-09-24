b3aa22872d1a288cc4885f9db6b0d14aa8144319
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-09-24 17:05:17 +0700

    config -> cleanup

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
-QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/fa5805751e44608b1e162.png")
-AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/fa5805751e44608b1e162.png")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME")
+BOT_USERNAME = getenv("BOT_USERNAME")
 
 admins = {}
 API_ID = int(getenv("API_ID"))
@@ -19,6 +19,6 @@ API_HASH = getenv("API_HASH")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! \ .").split())
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
